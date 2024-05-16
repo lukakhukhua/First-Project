f464c12e99d9988e03315e94bd83e3be7eead019
lukakhukhua <lkhuk21@freeuni.edu.ge>
2024-05-16 16:27:48 +0400

    updated addRegistryUser and ChangeRegistryUser to database functions.

--- src/main/java/com/others/firstProject/repositories/RegistryUserRepository.java
+++ src/main/java/com/others/firstProject/repositories/RegistryUserRepository.java
@@ -2,9 +2,16 @@ package com.others.firstProject.repositories;
 
 import com.others.firstProject.model.entity.RegistryUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RegistryUserRepository extends JpaRepository<RegistryUser, Long> {
+
+    @Query("SELECT ru.id FROM RegistryUser ru WHERE ru.RegId = :#{#registryUser.regId}")
+    Optional<Long> findIdByRegId(@Param("registryUser") RegistryUser registryUser);
 
 }
--- src/main/java/com/others/firstProject/services/RegistryServiceImpl.java
+++ src/main/java/com/others/firstProject/services/RegistryServiceImpl.java
@@ -3,6 +3,7 @@ package com.others.firstProject.services;
 import com.others.firstProject.model.entity.Registry;
 import com.others.firstProject.model.entity.RegistryUser;
 import com.others.firstProject.repositories.RegistryRepository;
+import com.others.firstProject.repositories.RegistryUserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,10 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class RegistryServiceImpl implements RegistryService {
+
+    private final RegistryRepository registryRepository;
+    private final RegistryUserRepository registryUserRepository;
+
 
     @Override
     @Transactional
@@ -26,8 +31,6 @@ public class RegistryServiceImpl implements RegistryService {
 
         System.out.println(registry + " has been added to the database");
     }
-
-    private final RegistryRepository registryRepository;
 
     @Override
     @Transactional
@@ -44,15 +47,44 @@ public class RegistryServiceImpl implements RegistryService {
         } else {
             throw new RuntimeException("Registry not found");
         }
+
+        System.out.println("The registry with id: " + registry.getId() + " has been changed");
     }
 
     @Override
+    @Transactional
     public void addRegistryUser(RegistryUser registryUser) throws Exception {
+        Optional<Long> existingRegistryId = registryUserRepository.findIdByRegId(registryUser);
 
+        if (existingRegistryId.isPresent()) {
+            throw new RuntimeException("Registry does not exist with RegId: " + registryUser.getRegId());
+        } else {
+            registryUserRepository.save(registryUser);
+        }
+
+        System.out.println(registryUser + " has been added to the database");
     }
 
     @Override
+    @Transactional
     public void changeRegistryUser(RegistryUser registryUser) throws Exception {
+        Optional<RegistryUser> existingRegistryUser = registryUserRepository.findById(registryUser.getId());
 
+        if (existingRegistryUser.isPresent()) {
+            RegistryUser existing = existingRegistryUser.get();
+
+            existing.setFirstName(registryUser.getFirstName());
+            existing.setLastName(registryUser.getLastName());
+            existing.setBirthDate(registryUser.getBirthDate());
+            existing.setMail(registryUser.getMail());
+            existing.setDescription(registryUser.getDescription());
+
+            registryUserRepository.save(existing);
+
+            System.out.println("The registry user with ID: " + existing.getId() + " has been changed");
+        } else {
+            throw new RuntimeException("RegistryUser not found with ID: " + registryUser.getId());
+        }
     }
+
 }
