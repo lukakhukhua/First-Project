b3e645ef602051aa3827c437a841e7fcb43fa3a5
lukakhukhua <lkhuk21@freeuni.edu.ge>
2024-05-16 14:32:37 +0400

    updated addRegistry and ChangeRegistry to database functions.

--- src/main/java/com/others/firstProject/repositories/RegistryRepository.java
+++ src/main/java/com/others/firstProject/repositories/RegistryRepository.java
@@ -2,9 +2,17 @@ package com.others.firstProject.repositories;
 
 import com.others.firstProject.model.entity.Registry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RegistryRepository extends JpaRepository<Registry, Long> {
-    
+
+    @Query("SELECT r.id FROM Registry r WHERE r.firstName = :#{#registry.firstName} AND r.lastName = :#{#registry.lastName} AND " +
+            "r.birthDate = :#{#registry.birthDate} AND r.relativeUserId = :#{#registry.relativeUserId}")
+    Optional<Long> findRegistryId(@Param("registry") Registry registry);
+
 }
--- src/main/java/com/others/firstProject/services/RegistryServiceImpl.java
+++ src/main/java/com/others/firstProject/services/RegistryServiceImpl.java
@@ -3,50 +3,47 @@ package com.others.firstProject.services;
 import com.others.firstProject.model.entity.Registry;
 import com.others.firstProject.model.entity.RegistryUser;
 import com.others.firstProject.repositories.RegistryRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RegistryServiceImpl implements RegistryService {
 
-    private final List<Registry> registryList;
+    @Override
+    @Transactional
+    public void addRegistry(Registry registry) throws Exception {
+        Optional<Long> existingRegistryId = registryRepository.findRegistryId(registry);
+
+        if (existingRegistryId.isPresent()) {
+            throw new RuntimeException("Registry already exists with ID: " + existingRegistryId.get());
+        } else {
+            registryRepository.save(registry);
+        }
+
+        System.out.println(registry + " has been added to the database");
+    }
+
     private final RegistryRepository registryRepository;
 
     @Override
-    public void addRegistry(Registry registry) throws Exception {
-        // TODO: add registry to the database
-
-        if (registryList.contains(registry)) {
-            throw new Exception("Registry already exists");
-        }
-
-        registryRepository.save(registry);
-        registryList.add(registry);
-
-        System.out.println(registry + " has been added to the database");
-
-        System.out.println("\n All registries: " + registryList);
-    }
-
-    @Override
+    @Transactional
     public void changeRegistry(Registry registry) throws Exception {
-        // TODO: change registry to the database
+        Optional<Registry> existingRegistry = registryRepository.findById(registry.getId());
 
-        boolean registryExists = registryList.remove(registry);
-
-        if (!registryExists) {
-            throw new Exception("No such registry exists");
+        if (existingRegistry.isPresent()) {
+            Registry existing = existingRegistry.get();
+            existing.setFirstName(registry.getFirstName());
+            existing.setLastName(registry.getLastName());
+            existing.setBirthDate(registry.getBirthDate());
+            existing.setRelativeUserId(registry.getRelativeUserId());
+            registryRepository.save(existing);
+        } else {
+            throw new RuntimeException("Registry not found");
         }
-        registryRepository.deleteById(registry.getId());
-
-        registryList.add(registry);
-
-        System.out.println(registry + " has been changed");
-
-        System.out.println("\n All registries: " + registryList);
     }
 
     @Override
